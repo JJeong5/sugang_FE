8d0a703683419b036390dc4aeb3b08de6f943aad
JJungMe <ojh4917@naver.com>
2024-01-10 12:16:10 +0900

    sed 명령어 방식으로 빌드번호 지정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,14 +5,14 @@ ECR_REPO="194453983284.dkr.ecr.ap-northeast-2.amazonaws.com"
 IMAGE_NAME="myreca"
 
 # 빌드 번호를 태그로 사용
-TAG="FE$BUILD_NUMBER"
+TAG="FE"
 
 REGION="ap-northeast-2"
 ACCOUNT_ID="194453983284"
 
 # ECR 이미지 가져오기
 aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com
-docker pull $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$ECR_REPO/$IMAGE_NAME:$TAG
+docker pull $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$IMAGE_NAME:$TAG
 
 # 현재 실행중인 컨테이너 확인
 CURRENT_CONTAINER_ID=$(docker ps -q --filter ancestor=$ECR_REPO --format="{{.ID}}")
@@ -25,4 +25,4 @@ if [ -n "$CURRENT_CONTAINER_ID" ]; then
 fi
 
 # 새로운 컨테이너 실행
-docker run -d -p 80:80 --name $IMAGE_NAME $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$ECR_REPO:$TAG
+docker run -d -p 80:80 --name $IMAGE_NAME $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$IMAGE_NAME:$TAG
