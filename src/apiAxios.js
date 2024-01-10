950b411d9dddeb5a53c41dcd47dc7a559e6bb958
JJungMe <ojh4917@naver.com>
2024-01-10 10:54:15 +0900

    코드 자동 정렬

--- src/apiAxios.js
+++ src/apiAxios.js
@@ -1,16 +1,16 @@
 import axios from "axios";
 
-export default function apiAxios(url, method, callback, CallbackERROR){
-    axios(
-        {
-            url: '/api' + url,
-            method: method,
-            baseURL: 'http://localhost:8080',
-            withCredentials: true,
-        }
-    ).then(function (response){
-        callback(response);
-    }).catch(function (response){
-        CallbackERROR(response);
+export default function apiAxios(url, method, callback, CallbackERROR) {
+  axios({
+    url: "/api" + url,
+    method: method,
+    baseURL: "http://localhost:8080",
+    withCredentials: true,
+  })
+    .then(function (response) {
+      callback(response);
+    })
+    .catch(function (response) {
+      CallbackERROR(response);
     });
 }
